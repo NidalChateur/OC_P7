97e726c939252f7041aa0d13206134e09bfce0a1
Nidal Chateur <nidalchateur@gmail.com>
2023-06-16 02:11:16 +0200

    bruteforce.py finished, awating mentor validation

--- bruteforce.py
+++ bruteforce.py
@@ -1,88 +1,65 @@
+from itertools import combinations
+import time
+
 import pandas as pd
 
 
-def load() -> dict:
+def load(path="./input/data.xlsx") -> dict:
     """read the table of data and convert it into dict"""
 
-    actions = {}
-    data_frame = pd.read_excel("./test.xlsx")
+    stocks = {}
+    data_frame = pd.read_excel(path)
     line_number = data_frame.shape[0]
     for i in range(line_number):
-        actions[data_frame.at[i, "name"]] = {
+        stocks[data_frame.at[i, "name"]] = {
             "price": data_frame.at[i, "price"],
             "profit": data_frame.at[i, "profit"],
         }
 
-    return actions
+    return stocks
 
 
-def add_1(binary_number: str) -> str:
-    """add 1 to a binary number and return the sum"""
+def possible_wallets(input_data: dict, max=500) -> list:
+    """Generate all possible wallets with a maximum constraint of [500]."""
 
-    decimal_number = int(str(binary_number), 2) + 2**0
+    data = input_data
+    cost, value = 0, 0
+    wallets = []
+    for i in range(0, len(data) + 1):
+        solutions = list(combinations(list(data.keys()), i))
+        for wallet in solutions:
+            for stock in wallet:
+                cost += data[stock]["price"]
+                value += data[stock]["price"] * (1 + data[stock]["profit"])
+            if cost <= max:
+                wallets.append([wallet, cost, value])
+            cost, value = 0, 0
 
-    return bin(decimal_number).replace("0b", "").zfill(len(load()))
-
-
-def max() -> str:
-    """determine the maximum number of possibilities in binary"""
-
-    number = ""
-    for i in range(len(load())):
-        number += "1"
-
-    return number
-
-
-def possibilities() -> list:
-    """check all possibilities without constraints"""
-
-    all_possibilities = []
-    possibility = add_1(0)
-    while possibility != add_1(max()):
-        combination = []
-        index = 1
-
-        for p in reversed(possibility):
-            if p == "1":
-                combination.append(
-                    [
-                        f"Action-{index}",
-                        load()[f"Action-{index}"]["price"],
-                        load()[f"Action-{index}"]["profit"],
-                    ]
-                )
-            index += 1
-
-        all_possibilities.append(combination)
-        possibility = add_1(possibility)
-
-    return all_possibilities
+    return wallets
 
 
-def sort_possibilities():
-    """sort all possibilities by profit with the price constraints < 500
-    then export the result as excel file"""
+def output(wallets: list):
+    """sort the wallets list and write the best wallet in an Excel file"""
 
-    all_combinations = []
-    index = 1
-    for p in possibilities():
-        benefit = 0
-        price = 0
-        for j in p:
-            price += j[1]
-            benefit += j[2]
-        if price < 500:
-            all_combinations.append([f"combi {index}", p, price, round(benefit, 2)])
-        index += 1
-    for h in sorted(all_combinations, key=lambda w: (-w[3])):
-        print(h)
-    data = [
-        {"actions": d[1], "price": d[2], "benefit": d[3]}
-        for d in sorted(all_combinations, key=lambda w: (-w[3]))
+    output_data = [
+        {"the best wallet": d[0], "cost": d[1], "value after 2 years": d[2]}
+        for d in sorted(wallets, key=lambda w: (-w[2]))[:1]
     ]
-    data_to_export = pd.DataFrame.from_records(data)
-    data_to_export.to_excel("./output.xlsx")
+    data_to_export = pd.DataFrame.from_records(output_data)
+    data_to_export.to_excel("./output/output_brute_force.xlsx", index=False)
 
 
-sort_possibilities()
+def brute_force():
+    """run the brute force method"""
+
+    start = time.time()
+    input_data = load()
+    wallets = possible_wallets(input_data)
+    output(wallets)
+    end = time.time()
+    print(f"\n{2**len(input_data)} combinaisons tested in {end-start} secs.\n")
+    print("Here is the best wallet found using the rigorous and exhaustive")
+    print("brute force method : ./output/output_brute_force.xlsx\n")
+
+
+brute_force()
